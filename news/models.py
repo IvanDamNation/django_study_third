ec1d68ae505c141bc19ab2e21001dc2f52739d23
IvanDamNation <ivan.suvalkin@gmail.com>
2022-03-10 23:36:40 +0300

    Add more models representation. Minor bugfixes

--- news/models.py
+++ news/models.py
@@ -19,9 +19,15 @@ class Author(models.Model):
         self.ratingAuthor = p_rat * 3 + c_rat
         self.save()
 
+    def __str__(self):
+        return '{}'.format(self.authorUser)
+
 
 class Category(models.Model):
     name = models.CharField(max_length=64, unique=True)
+
+    def __str__(self):
+        return '{}'.format(self.name)
 
 
 class Post(models.Model):
